198c8d49596385fbdd92793a3d266de0407c2e53
jahongir1808 <jahongir.developer18@gmail.com>
2023-03-22 16:37:56 +0500

    sadasd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/home/Home";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Product from "./components/product/Product";
+import Strategies from "./components/strategies/Strategies";
+import Price from "./components/price/Price";
+import Clients from "./components/clients/Clients";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Home />
+        <Product />
+        <Strategies />
+        <Price />
+        <Clients />
+      </main>
+      <Footer />
+    </>
   );
 }
 
